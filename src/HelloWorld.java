1b7cc30fc0ebb50d37ea146c3ab2b6d0bb3296b2
daibiao <daibiao007@163.com>
2020-04-28 10:07:57 +0800

    update second

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("this first message!");
 
+        System.out.println("this second message!");
+
     }
 }
